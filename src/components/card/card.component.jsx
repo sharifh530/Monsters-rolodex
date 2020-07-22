e3aac165636b88409390d90a0f3e5162dbd720ea
Sazzad Hossain Sharif <sharifh530@gmail.com>
2020-07-22 13:14:13 +0600

    Update card.component.jsx

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-import { Card } from '../card/card.component';
+import './card.styles.css';
 
-import './card-list.styles.css';
-
-export const CardList = props => (
-  <div className='card-list'>
-    {props.monsters.map(monster => (
-      <Card key={monster.id} monster={monster} />
-    ))}
+export const Card = props => (
+  <div className='card-container'>
+    <img
+      alt='monster'
+      src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+    />
+    <h2> {props.monster.name} </h2>
+    <p> {props.monster.email} </p>
   </div>
 );
